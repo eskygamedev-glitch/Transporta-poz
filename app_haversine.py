a7873e58cc1ea86261dc5bbc75318e666985d351
eskygamedev-glitch <esky.gamedev@gmail.com>
2025-11-07 15:31:59 +0200

    app_haversine
    Added check for correct station ways. Added filter to only need 1 letter and 1 "-" for knowing the way.

--- app_haversine.py
+++ app_haversine.py
@@ -15,9 +15,6 @@ def fetch_data():
         if len(a) < 5:
             continue
 
-        
-
-
         vir = removeIntigers(a[8])
         # micro gradi
         lon_mikro = float(a[2])
@@ -30,7 +27,7 @@ def fetch_data():
         print(f"Lat: {lat:.6f}, Lon: {lon:.6f}, Virziens: {vir}")
 
         # atrast tuvako/esoso pieturu
-        match = trySearch(lat, lon)
+        match = trySearch(lat, lon, vir)
         #if match is not None:
             #print(f"Autobuss atrodas pieturā: {match}")
         
@@ -48,12 +45,9 @@ def removeIntigers(vir):
             pass
         else:
             outputStr += char
+
     return outputStr
-
-
-
 # parbaudei(vai ir pieturaa)
-
 def haversine(lat1, lon1, lat2, lon2):
     R = 6363133  # Zemes radiuss(metros)
     phi1 = math.radians(lat1)
@@ -65,15 +59,15 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return R * c
 
-def trySearch(lat, lon):
-    MAX_DISTANCE_METERS = 30
+def trySearch(lat, lon, vir):
+    MAX_DISTANCE_METERS = 15
     closest_stop = None
     closest_distance = float("inf")  
 
     for stop in all_bus_stops:
-        stop_name, stop_lat, stop_lon = stop
+        stop_name, stop_lat, stop_lon, stop_way = stop
         distance = haversine(lat, lon, stop_lat, stop_lon)
-        if distance < closest_distance:
+        if distance < closest_distance and vir == stop_way:
             closest_distance = distance
             closest_stop = stop_name
 
@@ -82,62 +76,58 @@ def trySearch(lat, lon):
         print(f"Atrasta atbilstība: {closest_stop} ({closest_distance:.1f} m attālumā)")
         return closest_stop
     else:
-        print(f"Tuvāko {MAX_DISTANCE_METERS} m attālumā nav pieturu (tuvākā: {closest_stop} {closest_distance:.1f} m)")
+        #print(f"Tuvāko {MAX_DISTANCE_METERS} m attālumā nav pieturu (tuvākā: {closest_stop} {closest_distance:.1f} m)")
         return None
 
-
-
-
-
 all_bus_stops = [
-    ["sarkandaugava_station_bus_stop_a_b", 56.99569, 24.12903],
-    ["sarkandaugava_bus_stop_a_b", 56.99419, 24.12494],
-    ["sarkandaugava_bus_stop_b_a", 56.99487, 24.12679],
-    ["tilta_iela_bus_stop_b_a", 56.99232, 24.12559],
-    ["tilta_iela_bus_stop_a_b", 56.99287, 24.12413],
-    ["vitolu_iela_bus_stop_b_a", 56.99232, 24.12559],
-    ["vitolu_iela_bus_stop_a_b", 56.99287, 24.12413],
-    ["olaines_iela_bus_stop_b_a", 56.98653, 24.13385],
-    ["olaines_iela_bus_stop_a_b", 56.98733, 24.133],
-    ["traumatologijas_bus_stop_a_b", 56.98378, 24.13604],
-    ["traumatologijas_bus_stop_b_a", 56.98292, 24.13627],
-    ["laktas_iela_bus_stop_a_b", 56.97934, 24.13657],
-    ["laktas_iela_bus_stop_b_a", 56.9783, 24.13601],
-    ["ierednu_iela_bus_stop_a_b", 56.97434, 24.13557],
-    ["ierednu_iela_bus_stop_b_a", 56.97332, 24.13517],
-    ["eveles_iela_bus_stop_a_b", 56.97069, 24.1333],
-    ["eveles_iela_bus_stop_b_a", 56.96898, 24.13173],
-    ["palidzibas_iela_bus_stop_b_a", 56.96474, 24.12758],
-    ["bruninieku_iela_bus_stop_a_b", 56.96093, 24.1217],
-    ["bruninieku_iela_bus_stop_b_a", 56.96178, 24.12284],
-    ["emelngaila_iela_bus_stop_b_a", 56.95923, 24.11856],
-    ["raina_bulvaris_bus_stop_b_a", 56.95393, 24.10927],
-    ["central_tirgus_bus_stop_a_b", 56.94572, 24.11878],
-    ["centrala_stacija_bus_stop_a_b", 56.94755, 24.11816],
-    ["centrala_stacija_bus_stop_b_a", 56.94607, 24.1187],
-    ["merkela_iela_bus_stop_a_b", 56.94908, 24.11928],
-    ["inzenieru_iela_bus_stop_a_b", 56.95122, 24.11682],
-    ["inzenieru_iela_bus_stop_b_a", 56.9509, 24.1149],
-    ["makslas_muzejs_bus_stop_a_b", 56.95565, 24.11133],
-    ["makslas_muzejs_bus_stop_b_a", 56.95611, 24.11258],
-    ["lacplesa_iela_bus_stop_a_b", 56.95818, 24.11688],
-    ["lacplesa_iela_bus_stop_b_a", 56.94978, 24.13127],
-    ["francu_licejs_bus_stopa_a_b", 56.96357, 24.12596],
-    ["aloju_iela_bus_stopa_a_b", 56.96757, 24.13079]
+    ["sarkandaugava_station_bus_stop_a_b", 56.99569, 24.12903, "a-"],
+    ["sarkandaugava_bus_stop_a_b", 56.99419, 24.12494, "a-"],
+    ["sarkandaugava_bus_stop_b_a", 56.99487, 24.12679, "-a"],
+    ["tilta_iela_bus_stop_b_a", 56.99232, 24.12559, "-a"],
+    ["tilta_iela_bus_stop_a_b", 56.99287, 24.12413, "a-"],
+    ["vitolu_iela_bus_stop_b_a", 56.99232, 24.12559, "-a"],
+    ["vitolu_iela_bus_stop_a_b", 56.99287, 24.12413, "a-"],
+    ["olaines_iela_bus_stop_b_a", 56.98653, 24.13385, "-a"],
+    ["olaines_iela_bus_stop_a_b", 56.98733, 24.133, "a-"],
+    ["traumatologijas_bus_stop_a_b", 56.98378, 24.13604, "a-"],
+    ["traumatologijas_bus_stop_b_a", 56.98292, 24.13627, "-a"],
+    ["laktas_iela_bus_stop_a_b", 56.97934, 24.13657, "a-"],
+    ["laktas_iela_bus_stop_b_a", 56.9783, 24.13601, "-a"],
+    ["ierednu_iela_bus_stop_a_b", 56.97434, 24.13557, "a-"],
+    ["ierednu_iela_bus_stop_b_a", 56.97332, 24.13517, "-a"],
+    ["eveles_iela_bus_stop_a_b", 56.97069, 24.1333, "a-"],
+    ["eveles_iela_bus_stop_b_a", 56.96898, 24.13173, "-a"],
+    ["palidzibas_iela_bus_stop_b_a", 56.96474, 24.12758, "-a"],
+    ["bruninieku_iela_bus_stop_a_b", 56.96093, 24.1217, "a-"],
+    ["bruninieku_iela_bus_stop_b_a", 56.96178, 24.12284, "-a"],
+    ["emelngaila_iela_bus_stop_b_a", 56.95923, 24.11856, "-a"],
+    ["raina_bulvaris_bus_stop_b_a", 56.95393, 24.10927, "2-a"],
+    ["central_tirgus_bus_stop_a_b", 56.94572, 24.11878, "a-"],
+    ["centrala_stacija_bus_stop_a_b", 56.94755, 24.11816, "a-"],
+    ["centrala_stacija_bus_stop_b_a", 56.94607, 24.1187, "-a"],
+    ["merkela_iela_bus_stop_a_b", 56.94908, 24.11928, "a-"],
+    ["inzenieru_iela_bus_stop_a_b", 56.95122, 24.11682, "a-"],
+    ["inzenieru_iela_bus_stop_b_a", 56.9509, 24.1149, "-a"],
+    ["makslas_muzejs_bus_stop_a_b", 56.95565, 24.11133, "a-"],
+    ["makslas_muzejs_bus_stop_b_a", 56.95611, 24.11258, "-a"],
+    ["lacplesa_iela_bus_stop_a_b", 56.95818, 24.11688, "a-"],
+    ["lacplesa_iela_bus_stop_b_a", 56.94978, 24.13127, "-a"],
+    ["francu_licejs_bus_stopa_a_b", 56.96357, 24.12596, "a-"],
+    ["aloju_iela_bus_stopa_a_b", 56.96757, 24.13079, "a-"],
 ]
 
-delta = 0.0001
+
 sequence = "1,3,"
-invalidChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+invalidChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
 while True:
    
     fetch_data()
-#    trySearch(56.95122, 24.11682)
-    # findMatch(56.96357, 24.12596)
 
-    print("Waiting for 10 seconds...")
+
+    print("Waiting for 5 seconds...")
     print("\n")
-    time.sleep(10)
+    time.sleep(5)
 
 
 
